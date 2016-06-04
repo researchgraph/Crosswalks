002ba08373e3fd66e3ce59d8c03c62555f9b278d
Amir Aryani <amir.aryani@gmail.com>
2016-06-04 21:43:10 +1000

    added py script for grants

---
+++ sydney.edu.au/py/GrantXMLSchemaInclude.py
@@ -0,0 +1,21 @@
+rifdef=\
+   [ "publication",
+     [ "key",
+        [ "keytxt","sydney.edu.au/grant/",
+          "data",'fldData["GrantCode"]'
+        ],
+        "local_ID",
+        [ 
+            "data",'fldData["GrantCode"]'
+        ],
+        "title",
+        [ 
+            "data",'fldData["ProjectTitle"]'
+        ],
+        "participant_list",
+        [
+          "data",'fldData["FirstTenInvestigators"]'
+        ]
+
+      ]
+   ]
