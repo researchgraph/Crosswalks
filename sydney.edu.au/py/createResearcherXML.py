4b213d82d89d4810a342c467c27c608c7d1a2918
Keir <sample@email.com>
2016-06-15 07:44:13 +1000

    Added relaod and setdefaultencoding

--- sydney.edu.au/py/createResearcherXML.py
+++ sydney.edu.au/py/createResearcherXML.py
@@ -2,6 +2,9 @@ import sys,csv,os
 import pdb
 import codecs
 
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 # Author Keir Vaughan-Taylor     Mon Feb  1 11:37:37 AEDT 2016
 # Input Output files
 rawDatIn="sampleResearcher.csv"
