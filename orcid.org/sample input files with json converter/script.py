104ec08b96f461d27af06a81cbdaa8b50691ac0f
Seiya Shimizu <boy0914@gmail.com>
2016-11-20 20:53:58 +1100

    folder name changed

---
+++ orcid.org/sample input files with json converter/script.py
@@ -0,0 +1,14 @@
+import json
+import dicttoxml
+import os, subprocess
+import sys
+
+for file in os.listdir("."):
+	if file.endswith('.json'):
+		with open(file) as origin_file:
+			jsonData = json.load(origin_file)
+		xml = dicttoxml.dicttoxml(jsonData)
+
+		xml_file = open(file.rsplit(".json",1)[0] + '.xml',"w")
+		xml_file.write(xml)
+		xml_file.close()
