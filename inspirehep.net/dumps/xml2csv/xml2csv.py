f784c654216d6f2b10edfcf933961c8eda263af1
Seiya Shimizu <seiya9shimizu@gmail.com>
2017-05-08 21:57:55 +1000

    Update xml2csv.py

--- inspirehep.net/dumps/xml2csv/xml2csv.py
+++ inspirehep.net/dumps/xml2csv/xml2csv.py
@@ -25,25 +25,32 @@ for xmlfile in os.listdir('.'):
 for input_file in os.listdir('.'):
 	if input_file.endswith(".xml"):
 		numberOfFilesProcessed += 1
+		NoHeader = False
+		
+		if numberOfFilesProcessed == 0:
+			NoHeader = True
+		else:
+			NoHeader = False
+		
 		print("Converting " + input_file + " " + str(numberOfFilesProcessed) + " files out of " + str(numberOfFiles) + " completed")
 		tree = et.parse(input_file)
 		root = tree.getroot()
 		if root.findall('{http://researchgraph.org/schema/v2.0/xml/nodes}datasets'): 
-			converter = xml2csv(input_file,"./converted_versions/dataset/" + input_file.split('.xml')[0] + ".csv")
+			converter = xml2csv(input_file,"./converted_versions/dataset/" + input_file.split('.xml')[0] + ".csv",noheader=NoHeader)
 			converter.convert(tag="{http://researchgraph.org/schema/v2.0/xml/nodes}dataset")
 
 		if root.findall('{http://researchgraph.org/schema/v2.0/xml/nodes}grants'): 
-			converter = xml2csv(input_file,"./converted_versions/grant/" + input_file.split('.xml')[0] + ".csv")
+			converter = xml2csv(input_file,"./converted_versions/grant/" + input_file.split('.xml')[0] + ".csv",noheader=NoHeader)
 			converter.convert(tag="{http://researchgraph.org/schema/v2.0/xml/nodes}grant")
 
 		if root.findall('{http://researchgraph.org/schema/v2.0/xml/nodes}publications'): 
-			converter = xml2csv(input_file,"./converted_versions/publication/" + input_file.split('.xml')[0] + ".csv")
+			converter = xml2csv(input_file,"./converted_versions/publication/" + input_file.split('.xml')[0] + ".csv",noheader=NoHeader)
 			converter.convert(tag="{http://researchgraph.org/schema/v2.0/xml/nodes}publication")
 
 		if root.findall('{http://researchgraph.org/schema/v2.0/xml/nodes}researchers'): 	
-			converter = xml2csv(input_file,"./converted_versions/researcher/" + input_file.split('.xml')[0] + ".csv")
+			converter = xml2csv(input_file,"./converted_versions/researcher/" + input_file.split('.xml')[0] + ".csv",noheader=NoHeader)
 			converter.convert(tag="{http://researchgraph.org/schema/v2.0/xml/nodes}researcher")
 
 		if root.findall('{http://researchgraph.org/schema/v2.0/xml/nodes}relations'): 
-			converter = xml2csv(input_file,"./converted_versions/relation/" + input_file.split('.xml')[0] + ".csv")
+			converter = xml2csv(input_file,"./converted_versions/relation/" + input_file.split('.xml')[0] + ".csv",noheader=NoHeader)
 			converter.convert(tag="{http://researchgraph.org/schema/v2.0/xml/nodes}relation")
